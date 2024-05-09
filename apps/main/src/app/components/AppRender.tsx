187aa61845482157f3d3a0e1a6b21f6166e6544e
qiyuhang <qiyuhang@zhixue.com>
2024-05-09 18:32:54 +0800

    feat: 增加项目切换动画

--- apps/main/src/app/components/AppRender.tsx
+++ apps/main/src/app/components/AppRender.tsx
@@ -1,25 +1,40 @@
 import { useProjectSwitch } from '@/hooks/useProjectSwitch'
-import { useTransition, animated } from '@react-spring/web'
-import { useState } from 'react'
+import { useTransition, animated, type AnimatedProps } from '@react-spring/web'
+import { type CSSProperties } from 'react'
 interface AppProps {
   apps: Array<{
     key: string
     component: () => JSX.Element
   }>
 }
-
+type PageItem = (props: AnimatedProps<{ style: CSSProperties }>) => React.ReactElement
+interface PageProps {
+  key: string
+  component: PageItem
+}
 const AppRender: React.FC<AppProps> = ({ apps }) => {
   const { currentProject } = useProjectSwitch()
-  const app = apps.find(_App => _App.key === currentProject)
+  const _apps: PageProps[] = apps.map(_app => {
+    return {
+      key: _app.key,
+      component: ({ style }) => (
+        <animated.div style={{ ...style }}>
+          <_app.component></_app.component>
+        </animated.div>
+      )
+    }
+  })
+  const app = _apps.find(_App => _App.key === currentProject)
+  const transitions = useTransition(currentProject, {
+    from: { transform: 'translate3d(100%,0,0)' },
+    enter: { transform: 'translate3d(0%,0,0)' },
+    leave: { transform: 'translate3d(-100%,0,0)' }
+  })
   return (
     <>
-      <div className='app-wrapper'>
-        {app && (
-          <div>
-            <app.component></app.component>
-          </div>
-        )}
-      </div>
+      {transitions(style => {
+        return <div className='bg-blue'>{app && <app.component style={style}></app.component>}</div>
+      })}
     </>
   )
 }
--- apps/main/src/components/marco-container/index.tsx
+++ apps/main/src/components/marco-container/index.tsx
@@ -1,7 +1,10 @@
 const MarcoContainer: React.FC<{ src: string }> = ({ src }) => {
   return (
-    <div className='h-[calc(100vh-50px)] w-screen  bg-white box-border'>
-      <iframe src={src} className='h-full w-[calc(100vw)]' />
+    <div className='h-[calc(100vh-50px)] w-screen p-6   box-border'>
+      <iframe
+        src={src}
+        className='h-[calc(100vh-98px)] bg-white shadow-2xl shadow-dark rounded-2 w-[calc(100vw-48px)]'
+      />
     </div>
   )
 }
